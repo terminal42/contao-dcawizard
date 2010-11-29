5229fa52e939182752b5faba236c196ae061cf25
Andreas Schempp <andreas@schempp.ch>
2010-11-29 20:56:58 +0000

    Added workaround for issue in Chrome: http://code.google.com/p/chromium/issues/detail?id=52842

--- html/dcawizard_src.js
+++ html/dcawizard_src.js
@@ -285,15 +285,17 @@ provides: [Group]
 ...
 */
 
-var Group = new Class({
-
-	initialize: function(){
+var Group = new Class(
+{
+	initialize: function()
+	{
 		this.instances = Array.flatten(arguments);
 		this.events = {};
 		this.checker = {};
 	},
 
-	addEvent: function(type, fn){
+	addEvent: function(type, fn)
+	{
 		this.checker[type] = this.checker[type] || {};
 		this.events[type] = this.events[type] || [];
 		if (this.events[type].contains(fn)) return false;
@@ -304,7 +306,16 @@ var Group = new Class({
 		return this;
 	},
 
-	check: function(type, instance, i){
+	check: function(type, instance, i)
+	{
+		// Workaround for issue in Chrome: http://code.google.com/p/chromium/issues/detail?id=52842
+		if (i == null && instance == null)
+		{
+			instance = type[1];
+			i = type[2];
+			type = type[0];
+		}
+		
 		this.checker[type][i] = true;
 		var every = this.instances.every(function(current, j){
 			return this.checker[type][j] || false;
@@ -315,7 +326,6 @@ var Group = new Class({
 			event.call(this, this.instances, instance);
 		}, this);
 	}
-
 });
 
 
