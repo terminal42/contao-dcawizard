98e927519b896c4907147843b1ac93a7ae9c7cb1
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2013-11-12 12:54:55 +0100

    Do not show records without a tstamp (see #8)

--- widgets/DcaWizard.php
+++ widgets/DcaWizard.php
@@ -131,7 +131,7 @@ class DcaWizard extends \Widget
         $GLOBALS['TL_JAVASCRIPT']['dcawizard'] = sprintf('system/modules/dcawizard/assets/dcawizard%s.js', (($GLOBALS['TL_CONFIG']['debugMode']) ? '' : '.min'));
 
         // Get the available records
-        $objRecords = \Database::getInstance()->execute("SELECT * FROM {$this->foreignTable} WHERE pid={$this->currentRecord}" . ($this->listField ? " ORDER BY {$this->listField}" : ""));
+        $objRecords = \Database::getInstance()->execute("SELECT * FROM {$this->foreignTable} WHERE pid={$this->currentRecord} AND tstamp>0" . ($this->listField ? " ORDER BY {$this->listField}" : ""));
 
         if ($objRecords->numRows) {
             // Use the callback to generate the list
