d9cefce3c7d3d6bcd955e57b5f61c7f77c14e112
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2026-02-19 15:41:26 +0100

    CS

--- assets/dcawizard.js
+++ assets/dcawizard.js
@@ -1,7 +1,7 @@
 import './dcawizard.scss';
 
 import { Application } from '@hotwired/stimulus';
-import GroupWidgetController from './controller';
+import WidgetController from './controller';
 
 const application = Application.start();
-application.register('terminal42--dcawizard', GroupWidgetController);
+application.register('terminal42--dcawizard', WidgetController);
